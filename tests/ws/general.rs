3e181b52d4d32bab18cb6603afb19425e51dcf84
Lanikean <jose.bravo@bloxroute.com>
2025-01-15 06:55:07 -0700

    add examples & source code

--- tests/ws/general.rs
+++ tests/ws/general.rs
@@ -195,3 +195,18 @@ async fn test_get_account_balance_ws(owner_address: &str) -> Result<()> {
     client.close().await?;
     Ok(())
 }
+
+#[test_case(100; "max slots")]
+#[tokio::test]
+#[ignore]
+async fn test_get_leader_schedule_grpc(max_slots: u64) -> Result<()> {
+    let client = WebSocketClient::new(None).await?;
+    
+    let response = client.get_leader_schedule(max_slots).await?;
+    println!(
+        "Get Leader Schedule Response: {}",
+        serde_json::to_string_pretty(&response)?
+    );
+    
+    Ok(())
+}
